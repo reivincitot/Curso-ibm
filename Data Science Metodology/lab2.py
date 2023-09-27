d30d28a4e8a1c7454d2874bdf17a836423c1c48d
reivincitot <reivincitot@gmail.com>
2023-09-27 14:04:39 -0300

    Fin del lab 2 del curso data science methodology

--- Data Science Metodology/lab2.py
+++ Data Science Metodology/lab2.py
@@ -17,3 +17,12 @@ print(x)
 
 y = recipes_df.shape
 print
+
+# Nuestro data set consiste en 57.691 recetas, Cara fila representa una receta, y por cada receta su correspondiente cocina es documentada también con 384 ingredientes existentes en las recetas o no. Comienza con almendras y termina con zucchini
+
+# Ahora que la recolección de información esta completa, data science usan estadísticas descriptivas y técnicas de visualización para entender de mejor manera la información y familiarizarse con ella. Data scientist, explora la información para:
+
+# 1. Entender su contenido.
+# 2. Verificar su calidad.
+# 3. Encontrar un punto de vista interesante de forma preliminar
+# 4. Determinar si información necesaria es requerida o rellenar cualquier falta de información 
